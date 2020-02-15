e6df16e9b0c579da443a94bcbb48cd59e8d71086
ATThorsen <49302211+ATThorsen@users.noreply.github.com>
2020-02-15 15:19:10 +0100

    Add files via upload

---
+++ target/rest-jpa-devops-starter-1.0.1/color.js
@@ -0,0 +1,5 @@
+        function changeText(){
+            document.getElementById("d1").style.color = "green";
+            document.getElementById("d2").style.color = "red";
+            document.getElementById("d3").style.color = "blue";
+        }
